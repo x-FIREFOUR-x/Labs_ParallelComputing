29659dc7eb47ca171702b7c2830d7d0c4f584a51
Alex <olexandrpasalsky@gmail.com>
2023-04-03 02:01:04 +0300

    test

--- Lab2/Lab2/src/Main.java
+++ Lab2/Lab2/src/Main.java
@@ -1,19 +1,6 @@
-import StandardAlgoMultiplyMatrix.StandardAlgorithm;
-import TapeAlgoMultiplyMatrix.TapeAlgorithm;
-import Types.Matrix;
-
 public class Main {
     public static void main(String[] args) {
-        Matrix matrix1 = new Matrix(5, 5, 0, 10);
-        Matrix matrix2 = new Matrix(5, 5, 0, 10);
-
-        StandardAlgorithm standardAlgorithm = new StandardAlgorithm();
-        Matrix resultMatrix1 = standardAlgorithm.multiply(matrix1, matrix2);
-
-        TapeAlgorithm tapeAlgorithm = new TapeAlgorithm(5);
-        Matrix resultMatrix2 = tapeAlgorithm.multiply(matrix1, matrix2);
-
-        boolean equal = resultMatrix1.Equal(resultMatrix2);
-
+        TestAlgorithms test = new TestAlgorithms();
+        test.runTest(1000, 20);
     }
 }
---
+++ Lab2/Lab2/src/TestAlgorithms.java
@@ -0,0 +1,41 @@
+import StandardAlgoMultiplyMatrix.StandardAlgorithm;
+import TapeAlgoMultiplyMatrix.TapeAlgorithm;
+import Types.Matrix;
+
+public class TestAlgorithms {
+
+    public void runTest(int sizeMatrix, int countThread){
+        System.out.print("Size matrix: ");
+        System.out.println(sizeMatrix);
+        System.out.print("count Thread: ");
+        System.out.println(countThread);
+
+        Matrix matrix1 = new Matrix(sizeMatrix, sizeMatrix, 0, 1000);
+        Matrix matrix2 = new Matrix(sizeMatrix, sizeMatrix, 0, 1000);
+
+
+        // Not parallel algorithm multiply matrices
+        long startTime = System.currentTimeMillis();
+
+        StandardAlgorithm standardAlgorithm = new StandardAlgorithm();
+        Matrix resultMatrix1 = standardAlgorithm.multiply(matrix1, matrix2);
+
+        long endTime = System.currentTimeMillis();
+        System.out.print("Time working standard algo: ");
+        System.out.println(endTime - startTime);
+
+
+        // Tape algorithm multiply matrices
+        startTime = System.currentTimeMillis();
+
+        TapeAlgorithm tapeAlgorithm = new TapeAlgorithm(countThread);
+        Matrix resultMatrix2 = tapeAlgorithm.multiply(matrix1, matrix2);
+
+        endTime = System.currentTimeMillis();
+        System.out.print("Time working tape algo: ");
+        System.out.println(endTime - startTime);
+
+
+        boolean equal = resultMatrix1.Equal(resultMatrix2);
+    }
+}
