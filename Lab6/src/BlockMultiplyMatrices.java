bb4c07aabee0c526e4021b264a48967ba1414974
Alex <olexandrpasalsky@gmail.com>
2023-05-01 21:16:24 +0300

    Task1

---
+++ Lab6/src/BlockMultiplyMatrices.java
@@ -0,0 +1,99 @@
+import mpi.*;
+
+import static java.lang.System.exit;
+
+public class BlockMultiplyMatrices {
+    private static final int countRows = 2000;
+    private static final int countColumn = 2000;
+
+    private static final int TAG_MASTER = 1;
+    private static final int TAG_WORKER = 2;
+
+    public static void main(String[] args) {
+        Matrix matrix1 = new Matrix(countRows, countColumn, 0, 10);
+        Matrix matrix2 = new Matrix(countRows, countColumn, 0, 10);
+        Matrix resultMatrix = new Matrix(countRows, countColumn);
+        double startTime, endTime;
+
+        MPI.Init(args);
+
+        int countTasks = MPI.COMM_WORLD.Size();
+        int taskID = MPI.COMM_WORLD.Rank();
+        //System.out.println("Tasks: "+ countTasks);
+
+        int masterID = 0;
+        int countWorkers = countTasks - 1;
+
+        if(countTasks < 2){
+            MPI.COMM_WORLD.Abort(1);
+            exit(1);
+        }
+
+        if(taskID == masterID){
+            startTime = MPI.Wtime();
+            int rowsForOneWorker = countRows / countWorkers;
+            int extraRows = countRows % countWorkers;
+
+            for (int i = 1; i <= countWorkers; i++) {
+                int indexStartRow = (i-1) * rowsForOneWorker;
+                int indexEndRow = indexStartRow + rowsForOneWorker - 1;
+                if(i == countWorkers){
+                    indexEndRow += extraRows;
+                }
+
+                Matrix subMatrix1 = matrix1.getSubMatrix(indexStartRow, indexEndRow, countColumn);
+                byte[] subMatrix1Buff = subMatrix1.convertToByteArray();
+                byte[] matrix2Buff = matrix2.convertToByteArray();
+
+                MPI.COMM_WORLD.Send(new int[]{indexStartRow},0, 1, MPI.INT, i, TAG_MASTER);
+                MPI.COMM_WORLD.Send(new int[]{indexEndRow},0, 1, MPI.INT, i, TAG_MASTER);
+                MPI.COMM_WORLD.Send(subMatrix1Buff,0, subMatrix1Buff.length , MPI.BYTE, i, TAG_MASTER);
+                MPI.COMM_WORLD.Send(matrix2Buff,0, matrix2Buff.length, MPI.BYTE, i, TAG_MASTER);
+            }
+
+
+            for (int i = 1; i <= countWorkers; i++) {
+                int[] indexStartRow = new int[1];
+                int[] indexEndRow = new int[1];
+                MPI.COMM_WORLD.Recv(indexStartRow,0,1, MPI.INT, i, TAG_WORKER);
+                MPI.COMM_WORLD.Recv(indexEndRow,0,1, MPI.INT, i, TAG_WORKER);
+
+                int countElemsResultBuffer = (indexEndRow[0]-indexStartRow[0] + 1) * countColumn * Integer.BYTES;
+                byte[] resultMatrixBuff = new byte[countElemsResultBuffer];
+                MPI.COMM_WORLD.Recv(resultMatrixBuff,0,
+                        countElemsResultBuffer , MPI.BYTE, i, TAG_WORKER);
+                Matrix subMatrix = new Matrix(resultMatrixBuff, indexEndRow[0] - indexStartRow[0] + 1, countColumn);
+
+                resultMatrix.putSubMatrix(subMatrix, indexStartRow[0], indexEndRow[0], countColumn);
+            }
+            endTime = MPI.Wtime();
+            //resultMatrix.print();
+            System.out.println(endTime-startTime + " seconds");
+        }
+        else {
+            int[] indexStartRow = new int[1];
+            int[] indexEndRow = new int[1];
+            MPI.COMM_WORLD.Recv(indexStartRow,0,1, MPI.INT, 0, TAG_MASTER);
+            MPI.COMM_WORLD.Recv(indexEndRow,0,1, MPI.INT, 0, TAG_MASTER);
+
+            int sizeSubMatrix1Buff = (indexEndRow[0] - indexStartRow[0] + 1) * countColumn * Integer.BYTES;
+            int sizeMatrix2Buff = countRows * countColumn * Integer.BYTES;
+            byte[] subMatrix1Buff = new byte[sizeSubMatrix1Buff];
+            byte[] matrix2Buff = new byte[sizeMatrix2Buff];
+            MPI.COMM_WORLD.Recv(subMatrix1Buff,0, sizeSubMatrix1Buff, MPI.BYTE,0, TAG_MASTER);
+            MPI.COMM_WORLD.Recv(matrix2Buff,0,sizeMatrix2Buff, MPI.BYTE,0, TAG_MASTER);
+
+            Matrix subMatrix1 = new Matrix(subMatrix1Buff, indexEndRow[0] - indexStartRow[0] + 1, countColumn);
+            Matrix Matrx2 = new Matrix(matrix2Buff, countRows, countColumn);
+            Matrix resultMatrx = subMatrix1.multiply(Matrx2);
+
+            byte[] resultMatrixBuff = resultMatrx.convertToByteArray();
+
+            MPI.COMM_WORLD.Send(indexStartRow,0, 1, MPI.INT, 0, TAG_WORKER);
+            MPI.COMM_WORLD.Send(indexEndRow,0, 1, MPI.INT, 0, TAG_WORKER);
+            MPI.COMM_WORLD.Send(resultMatrixBuff,0, resultMatrixBuff.length, MPI.BYTE, 0, TAG_WORKER);
+        }
+
+        MPI.Finalize();
+    }
+}
---
+++ Lab6/src/Matrix.java
@@ -0,0 +1,156 @@
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Objects;
+import java.util.Random;
+
+public class Matrix {
+    private int[][] data;
+
+    private int countRows;
+    private int countColumns;
+
+    public Matrix(int countRows, int countColumns){
+        this.countRows = countRows;
+        this.countColumns = countColumns;
+        data = new int[countRows][countColumns];
+    }
+    public Matrix(int countRows, int countColumns, int randomMin, int randomMax){
+        this(countRows, countColumns);
+
+        Random random = new Random();
+        for (int i = 0; i < countRows; i++) {
+            for (int j = 0; j < countColumns; j++) {
+                int numb = (random.nextInt(randomMax - randomMin + 1)) + randomMin;
+                data[i][j] = numb;
+            }
+        }
+    }
+
+    public int getCountRows() {
+        return countRows;
+    }
+    public int getCountColumns() {
+        return  countColumns;
+    }
+
+    public int getElement(int idxRow, int idxColumn)
+    {
+        return data[idxRow][idxColumn];
+    }
+    public void setElement(int idxRow, int idxColumn, int element) {
+        data[idxRow][idxColumn] = element;
+    }
+
+    public int[] getRow(int index){
+        return data[index];
+    }
+    public void setRow(int index, int[] row){
+        data[index] = row;
+    }
+
+    public int[] getColumn(int index){
+        int[] column = new int[countColumns];
+        for (int i = 0; i < getCountRows(); i++) {
+            column[i] = (data[i][index]);
+        }
+        return column;
+    }
+
+    public boolean Equal(Matrix matrix){
+        if (this.getCountColumns() != matrix.getCountRows() || this.getCountRows() != matrix.getCountColumns())
+            return false;
+
+        for (int i = 0; i < matrix.getCountRows(); i++) {
+            for (int j = 0; j < matrix.getCountColumns(); j++) {
+                if (!Objects.equals(this.data[i][j], matrix.getElement(i, j)))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    public Matrix clone()
+    {
+        Matrix clone = new Matrix(this.countRows, this.countColumns);
+        for (int i = 0; i < clone.countRows; i++) {
+            for (int j = 0; j < clone.countColumns; j++) {
+                clone.setElement(i, j, this.data[i][j]);
+            }
+        }
+        return clone;
+    }
+
+    public void print(){
+        StringBuilder buf = new StringBuilder();
+        for (int i = 0; i < countRows; i++) {
+            for (int j = 0; j < countColumns; j++) {
+                buf.append(data[i][j]).append(" ");
+            }
+            buf.append("\n");
+        }
+        System.out.println(buf);
+    }
+
+    public Matrix multiply(Matrix matrix2) {
+        Matrix result = new Matrix(this.getCountRows(), matrix2.getCountColumns());
+        for (int i = 0; i < this.getCountRows(); i++) {
+            for (int j = 0; j < matrix2.getCountColumns(); j++) {
+                int element = 0;
+                for (int k = 0; k < matrix2.getCountRows(); k++) {
+                    element += this.getElement(i, k) * matrix2.getElement(k, j);
+                }
+                result.setElement(i, j, element);
+            }
+        }
+        return result;
+    }
+
+    public Matrix getSubMatrix(int indexStartRow, int indexEndRow, int countColumns)
+    {
+        Matrix subMatrix = new Matrix(indexEndRow - indexStartRow + 1, countColumns);
+        for (int i = indexStartRow; i <= indexEndRow; i++) {
+            for (int j = 0; j < countColumns; j++) {
+                subMatrix.setElement(i - indexStartRow, j, data[i][j]);
+            }
+        }
+        return subMatrix;
+    }
+
+    public void putSubMatrix(Matrix matrix, int indexStartRow, int indexEndRow, int countColumns)
+    {
+        for (int i = indexStartRow; i <= indexEndRow; i++) {
+            for (int j = 0; j < countColumns; j++) {
+                data[i][j] = matrix.getElement(i - indexStartRow, j);
+            }
+        }
+    }
+
+    public byte[] convertToByteArray()
+    {
+        ByteBuffer buffer = ByteBuffer.allocate(countRows * countColumns * Integer.BYTES);
+        buffer.order(ByteOrder.nativeOrder());
+        for (int i = 0; i < countRows; i++) {
+            for (int j = 0; j < countColumns; j++) {
+                buffer.putInt(data[i][j]);
+            }
+        }
+        return buffer.array();
+    }
+
+    public Matrix(byte[] bytes, int countRows, int countColumns){
+        this.countRows = countRows;
+        this.countColumns = countColumns;
+        data = new int[countRows][countColumns];
+
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        buffer.order(ByteOrder.nativeOrder());
+        for (int i = 0; i < countRows; i++) {
+            for (int j = 0; j < countColumns; j++) {
+                data[i][j] = buffer.getInt();
+            }
+        }
+    }
+
+
+}
